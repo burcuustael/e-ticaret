f3c5cc28189ed533f52feb06e15fcb0d0f355cff
burcuustael <burcustael@gmail.com>
2024-08-18 17:01:54 +0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import path from 'path';
+import { LayoutsComponent } from './components/layouts/layouts.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: LayoutsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
